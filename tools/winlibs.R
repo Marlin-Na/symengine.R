ee9674552d3b6fc9d2118958d0bec9628cd5f0e0
Marlin- <marlin-@gmx.cn>
2018-03-09 20:03:50 +0800

    Download mpir

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -8,6 +8,9 @@ mpc_win64 <- "https://anaconda.org/conda-forge/mpc/1.0.3/download/win-64/mpc-1.0
 mpfr_win32 <- "https://anaconda.org/conda-forge/mpfr/3.1.5/download/win-32/mpfr-3.1.5-vc14_0.tar.bz2"
 mpfr_win64 <- "https://anaconda.org/conda-forge/mpfr/3.1.5/download/win-64/mpfr-3.1.5-vc14_0.tar.bz2"
 
+mpir_win32 <- "https://anaconda.org/conda-forge/mpir/3.0.0/download/win-32/mpir-3.0.0-vc14_0.tar.bz2"
+mpir_win64 <- "https://anaconda.org/conda-forge/mpir/3.0.0/download/win-64/mpir-3.0.0-vc14_0.tar.bz2"
+
 is_32 <- function () {
     machine <- Sys.info()[["machine"]]
     
@@ -64,11 +67,27 @@ download_mpfr <- function () {
     unlink("mpfrlib.tar.bz2")
 }
 
+download_mpir <- function () {
+    if (file.exists("../win-mpir/Library/lib/gmp.lib"))
+        return()
+    
+    if (is_32())
+        link <- mpir_win32
+    else
+        link <- mpir_win64
+    download.file(link, "mpirlib.tar.bz2", quiet = FALSE)
+    if (!dir.exists("../win-mpir"))
+        dir.create("../win-mpir")
+    untar("mpirlib.tar.bz2", exdir = "../win-mpir", tar = "internal")
+    unlink("mpirlib.tar.bz2")
+}
+
 # Ensure we are in the correct directory
 stopifnot(file.exists("../DESCRIPTION"))
 
 download_symengine()
 download_mpc()
 download_mpfr()
+download_mpir()
 
 

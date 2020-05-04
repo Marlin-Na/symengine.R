d3cf8f1c11caa811a98c0edb697ae72dca5522c0
Jialin Ma <marlin@inventati.org>
2020-05-04 02:19:46 -0400

    Update experiments.R

--- experiments.R
+++ experiments.R
@@ -2,31 +2,48 @@
 library(reticulate)
 library(symengine)
 
-reticulate::use_python(Sys.which("python3"), required = TRUE)
+reticulate::use_virtualenv("r-reticulate", required = TRUE)
+
+#reticulate::py_discover_config()
+#reticulate::py_config()
+
+reticulate::py_install("Cython")
+reticulate::py_install("sympy")
+symengine:::symengine_py_install(ref = "Marlin-Na/symengine.py@pycapsule")
+
+s4basic_as_py <- function(x, convert=FALSE) {
+    symengine_py_module <- reticulate::import("symengine", convert=FALSE)
+    ## Convert external pointer to pycapsule
+    cap <- reticulate::r_to_py(x@ptr, convert=convert)
+    ## Convert to symengine py object
+    ans <- symengine_py_module$lib$symengine_wrapper$sympify_pycapsule(cap)
+    ans
+}
+
+s4basic_from_py <- function(x) {
+    symengine_py_module <- reticulate::import("symengine", convert=FALSE)
+    ans <- symengine:::s4basic()
+    symengine_py_module$lib$symengine_wrapper$assign_to_pycapsule(
+        reticulate::r_to_py(ans@ptr), x)
+    ans
+}
+
+r_to_py.Basic <- function(x, convert=FALSE) {
+    s4basic_as_py(x, convert)
+}
+
+py_to_r.symengine.lib.symengine_wrapper.Basic <- function(x) {
+    s4basic_from_py(x)
+}
+
+py_to_r.sympy.core.basic.Basic <- function(x) {
+    s4basic_from_py(x)
+}
+
 
 x <- S("x")
 
-s4basic_as_py <- function(x) {
-    ## Convert external pointer to pycapsule
-    cap <- reticulate::r_to_py(x@ptr)
-
-    ## Extract pointer from capsule
-    ctypes <- reticulate::import("ctypes", convert=FALSE)
-    PyCapsule_GetPointer <- ctypes$pythonapi$PyCapsule_GetPointer
-    PyCapsule_GetPointer$restype <- ctypes$c_void_p
-    PyCapsule_GetPointer$argtypes <- list(ctypes$py_object, ctypes$c_char_p)
-    handle <- PyCapsule_GetPointer(cap, NULL)
-    handle # python int
-
-    ## Cast into ctypes pointer
-    py_basic_voidp <- ctypes$cast(handle, ctypes$c_void_p)
-    
-    ## TODO
-}
-
-symengine_dll <- ctypes$cdll$LoadLibrary("symengine.so")
-symengine_dll$basic_abs
-symengine_dll$basic_new_stack
-
-symengine_py <- reticulate::import("symengine", convert=FALSE)
+sympy <- reticulate::import("sympy")
+sympy$integrate(x^2L/2L)
+sympy$integrate(x^2L/2L, tuple(x, 1L, 2L))
 
